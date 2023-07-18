e8efb1bcb9261946889ea5a50cca1480af90e801
Brian Griffin <wbgriffin@pm.me>
2023-07-17 21:35:26 -0400

    feat: add decorative header

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div className={styles.app}>
-        <p>App</p>
+        <header />
         <ul className={styles.list}>
           {listings}
         </ul>
